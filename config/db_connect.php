a22bb9807a4569d9d4809580916aec7bda918f93
preciouskayili <preciouskayili@gmail.com>
2021-10-20 00:19:47 +0100

    Final commit before db

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,9 +1,15 @@
 <?php
-// Connection data
-$host = "localhost";
-$database = "heypresh";
-$username = "Presh";
-$password = "Legendpresh88!!";
+// Deployed connection data.
+// mysql://bd996b9b473ae7:df6bd8ff@us-cdbr-east-04.cleardb.com/heroku_88e7d26b13e0d22?reconnect=true
 
-// Connection to database
-$conn = mysqli_connect($host, $username, $password, $database);
+// Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = true;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
